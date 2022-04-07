bbb7adf38782b452ee233100b75d6d81b2740574
lemonhandgrenade <36611142+lemonhandgrenade@users.noreply.github.com>
2022-04-07 13:12:17 +1000

    Uneeded
    go away

--- data/hp/functions/spells/test.py
+++
@@ -1,6 +0,0 @@
-import os
-
-folders = ['h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'custom']
-for x in folders:
-	if not os.path.isdir(x):
-		os.mkdir('./' + x)
